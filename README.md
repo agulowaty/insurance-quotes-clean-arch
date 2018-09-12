975b4ecaafa44eae888e431b08be1b80d7d04e54
Artur Gulowaty <artur.gulowaty@bjss.com>
2018-09-12 10:49:01 +0100

    initial commit with Spring app stubs

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# insurance-quotes-clean-arch
+# Clean architecture excercise for inusrance domain
 Clean architecture code kata for simple insurance quote retrieval problem
---
+++ src/main/kotlin/com/gulowaty/insurancequotescleanarch/InsuranceQuotesCleanArchApplication.kt
@@ -0,0 +1,11 @@
+package com.gulowaty.insurancequotescleanarch
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class InsuranceQuotesCleanArchApplication
+
+fun main(args: Array<String>) {
+    runApplication<InsuranceQuotesCleanArchApplication>(*args)
+}
